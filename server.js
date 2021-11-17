ce0c52abff1aed12807a11967815099752692535
Claire <clairehh@live.unc.edu>
2021-11-17 12:16:16 -0500

    another commit

--- server.js
+++ server.js
@@ -45,18 +45,17 @@ app.get("/app/user/:id", (req, res) => {
 });
 
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
-app.patch("/app/update/user:id", (req, res) => {	
-	const stmt = db.prepare("SELECT * FROM userinfo").all();
-	for (const user of stmt.iterate()) {
-		console.log("here")
-	}
-	res.status(200).json(stmt);
+app.patch("/app/update/user/:id", (req, res) => {	
+	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?");
+	const info = stmt.run(req.body.user, md5(req.body.pass), req.params.id);
+	res.status(200).json({"message": info.changes + " record updated: ID " + req.params.id + " (200)"});
 });
 
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
 app.delete("/app/delete/user/:id", (req, res) => {	
-	const stmt = db.prepare("SELECT * FROM userinfo").all();
-	res.status(200).json(stmt);
+	const stmt = db.prepare("DELETE FROM userinfo WHERE id = ?");
+	const info = stmt.run(req.params.id);
+	res.status(200).json({"message": info.changes + " record deleted: ID " + req.params.id + " (200)"});
 });
 
 // Default response for any other request
