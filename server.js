b4a2569b84715a0b3c9439698a3c57d88efdcffe
Claire <clairehh@live.unc.edu>
2021-11-17 12:07:09 -0500

    post?

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get("/app/", (req, res, next) => {
 app.post("/app/new", (req, res) => {	
 	const stmt = db.prepare('INSERT INTO userinfo (user, pass) VALUES (?, ?)').all();
 	const info = stmt.run(req.body.user, md5(req.body.pass));
-	res.status(201).json(stmt);
+	res.status(201).json({"message": info.changes+ " record created: ID " + info.req.params.id + " (201)"});
 });
 
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
